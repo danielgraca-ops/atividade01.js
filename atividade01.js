f0ab4d9791f3233791b8f5d07def6bfad685afc8
DANIEL ROSA DA GRAÇA <daniel_graca@estudante.sesisenai.org.br>
2026-09-21 20:45:26 -0300

    Ficha de Item

---
+++ atividade01.js
@@ -0,0 +1,10 @@
+const nomeITEM = "Epadada das sombras";
+const precoItem = 250
+const raridadeItem = "Raro"
+const estoqueItem = 12;
+
+console.log (`
+=== FICHA DO ITEM ===
+nomeITEM: ${nomeIntem}  
+preço: R${precoItem}
+raridade; ${raridadeItem} unidades `):
