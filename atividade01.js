241652175cf8f8e329d52ce1fa1034d6e77c8e17
DANIEL ROSA DA GRAÇA <daniel_graca@estudante.sesisenai.org.br>
2026-09-21 22:17:16 -0300

    preço

--- atividade01.js
+++ atividade01.js
@@ -7,19 +7,17 @@ console.log (`
 === FICHA DO ITEM ===
 nomeITEM: ${nomeIntem}  
 preço: R${precoItem}
-<<<<<<< HEAD
-raridade; ${raridadeItem} unidades `)
-=======
-raridade; ${raridadeItem} unidades `):
+
+raridade; ${raridadeItem} unidades 
 
 //validação do preço
-if (precoItem < 0) 
-    console.log("Classificação: Comum");
-} else if (precoItem >= 100 && precoItem < 500) {
-    console.log("Classificação: Lendario");
+if (precoItem < 0) {
+    console.log("Classificação: Comum")
+ } else if (precoItem >= 100 && precoItem < 500) {
+    console.log("Classificação: Lendario"):
 }
 
 //Operador ternário
-const emDestaque = precoItem > 500 ? true : false
+const emDestaque = precoItem > 500 ? true : false:
 
-console.log('Em destaque: $ {emDestaque}')
+console.log('Em destaque: $ {emDestaque}`)
